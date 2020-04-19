5b1b54c1c81305fd551bf1a2ca42c94541d36b36
sagar  tripathi <tripathisagar1997@gmail.com>
2020-04-19 15:48:12 +0530

    GridView of FLP project

--- app/src/main/java/com/example/flp/MainActivity.java
+++ app/src/main/java/com/example/flp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.flp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void history(View view) {
+
+        Intent intent = new Intent(this, history_flp.class);
+        startActivity(intent);
+
+    }
+
+
+    public void approvel(View view) {
+        Intent intent = new Intent(this, approval_flp.class);
+        startActivity(intent);
+
+
+    }
+
+    public void marketing(View view) {
+        Intent intent = new Intent(this, marketing_flp.class);
+        startActivity(intent);
+
+    }
+    public void start(View view) {
+        Intent intent = new Intent(this, starting_flp.class);
+        startActivity(intent);
+
+    }
+
+    public void ranking(View view) {
+        Intent intent = new Intent(this, benefit_flp.class);
+        startActivity(intent);
+
+    }
+
+    public void benefit(View view) {
+        Intent intent = new Intent(this, rank_flp.class);
+        startActivity(intent);
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/flp/history_flp.java
@@ -0,0 +1,14 @@
+package com.example.flp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class history_flp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_flp);
+    }
+}
