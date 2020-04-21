a750cb20e3e85c4450c51c7e8db37000a8fe5241
sagar  tripathi <tripathisagar1997@gmail.com>
2020-04-21 12:47:50 +0530

    after fifth complet

--- app/src/main/java/com/example/flp/MainActivity.java
+++ app/src/main/java/com/example/flp/MainActivity.java
@@ -5,32 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
-
-    public void history(View view) {
-
-        Intent intent = new Intent(this, history_flp.class);
-        startActivity(intent);
-
-    }
-
-
-    public void approvel(View view) {
-        Intent intent = new Intent(this, approval_flp.class);
-        startActivity(intent);
-
-
-    }
-
     public void marketing(View view) {
         Intent intent = new Intent(this, marketing_flp.class);
         startActivity(intent);
@@ -55,4 +54,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void approvel(View view) {
+        Intent intent = new Intent(this, approval_flp.class);
+        startActivity(intent);
+    }
+
+    public void history(View view) {
+        Intent intent = new Intent(this, history_flp.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/flp/rank_flp.java
+++ app/src/main/java/com/example/flp/rank_flp.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class rank_flp extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
+public class rank_flp extends AppCompatActivity {
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank_flp);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView = findViewById(R.id.adView2);
+        AdRequest adRequest2 = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest2);
+
     }
 }
